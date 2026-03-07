f84230eba17234e783e3bbd1e7b99dac8bec7ff8
Shruti <shruti6834@gmail.com>
2026-03-07 18:37:45 +0530

    password generator function
    Add a password generator function that creates a random password using letters, digits, and symbols.

--- Week-4/password_genrator.py
+++ Week-4/password_genrator.py
@@ -1,1 +1,17 @@
+import random
+import string
+letters = string.ascii_letters
+digits = string.digits
+symbols = string.punctuation
+characters = letters + digits + symbols
 
+def password_genrator():
+    length = int(input("Enter Password Length: "))
+    if length <= 0:
+        print("Enter a positive length")
+    password =""
+    for i in range(length):
+        password += random.choice(characters)
+    print(f"Generated Password: {password}") 
+
+password_genrator()
